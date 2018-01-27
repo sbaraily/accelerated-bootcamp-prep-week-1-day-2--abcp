c47f8d85802a7aeaba6a4da2731917101d6ad46f
sbaraily <samir.baraily@gmail.com>
2018-01-27 04:03:40 +0000

    automatically backed up by learn

--- array.js
+++ array.js
@@ -18,7 +18,7 @@ function destructivelyAddElementToEndOfArray(array, element) {
 }
 
 function accessElementInArray(array, index) {
-
+return array[index]
 }
 
 function destructivelyRemoveElementFromBeginningOfArray(array) {
